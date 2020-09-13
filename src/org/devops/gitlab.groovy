cef379c013e24473ea4c98f336a796099cf66b94
qin.zehua <775935087@qq.com>
2020-09-13 21:14:31 +0800

    更改git commit状态

---
+++ src/org/devops/gitlab.groovy
@@ -0,0 +1,23 @@
+package org.devops
+
+def HttpReq(reqType, reqUrl, reqBody) {
+    def gitServer = 'http://192.168.0.102:13800/ap1/v4'
+    withCredentials([string(credentialsId: 'gitlab-token', variable: 'gitlabToken')]) {
+        result = httpRequest customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: "${gitlabToken}"]],
+                 httpMode: reqType,
+                 contentType: 'APPLICATION_JSON',
+                 consoleLogResponseBody: true,
+                 ignoreSslErrors: true,
+                 requestBody: reqBody,
+                 url: "${gitServer}/${reqUrl}"
+    }
+    return result
+}
+
+// 更改commit状态
+def ChangeCommitStatus(projectId, commitSha, status) {
+    commitApi = "projects/${projectId}/status/${commitSha}?state=${status}"
+    response = HttpReq('POST', commitApi, '')
+    println(response)
+    return response
+}
