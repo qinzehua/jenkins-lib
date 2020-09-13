15a3d4451e01b2c7e607f769cb2b757d70de6da1
qin.zehua <775935087@qq.com>
2020-09-13 09:12:24 +0800

    update

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -2,7 +2,7 @@ package org.devops
 
 //构建类型
 def Buid(buildType, buildShell) {
-    def buildTools = ['maven': 'M2', 'ant': 'ANT', 'gradle': 'GRADLE', 'npm': 'NPM']
+    def buildTools = ['mvn': 'M2', 'ant': 'Ant', 'gradle': 'GRADLE', 'npm': 'NodeEnv']
     buildHome = tool buildTools[buildType]
     println('当前选的build类型是 ${buildType}')
     sh "${buildHome}/bin/${buildType} ${buildShell}"
