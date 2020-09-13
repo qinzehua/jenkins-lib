e821ef598e8225b574deda5b6d7d6d1c4bd6991c
qin.zehua <775935087@qq.com>
2020-09-13 22:24:17 +0800

    api 写错

--- src/org/devops/gitlab.groovy
+++ src/org/devops/gitlab.groovy
@@ -1,7 +1,7 @@
 package org.devops
 
 def HttpReq(reqType, reqUrl, reqBody) {
-    def gitServer = 'http://192.168.0.102:13800/ap1/v4'
+    def gitServer = 'http://192.168.0.102:13800/api/v4'
     withCredentials([string(credentialsId: 'global-gitlab-token', variable: 'gitlabToken')]) {
         result = httpRequest customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: "${gitlabToken}"]],
                  httpMode: reqType,
