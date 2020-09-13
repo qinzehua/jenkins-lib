70dbd0562bdc31a95d5b1202d3fe0b4db9f0720d
qin.zehua <775935087@qq.com>
2020-09-13 14:27:19 +0800

    ansible

--- src/org/devops/deploy.groovy
+++ src/org/devops/deploy.groovy
@@ -1,5 +1,11 @@
 package org.devops
 
+//saltstack
 def SaltDeploy(host, func) {
     sh "salt \"${host}\" ${func}"
+}
+
+//absible
+def AnsibleDeploy(host, func) {
+    sh "ansible ${func} ${host}"
 }
