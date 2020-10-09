90a28719894c09bbade22c8120f335bb05355717
qin.zehua <775935087@qq.com>
2020-10-09 20:04:54 +0800

    更新扫描脚本

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -1,12 +1,14 @@
 package org.devops
 
 def SonarScan(projectName, des, path) {
+    // withSonarQubeEnv('sonarqube-test')
     def scannerHome = '/usr/local/sonar-scanner-4.4.0.2170-linux/'
     def sonarServer = 'http://192.168.0.102:9000'
     sonarDate = sh returnStdout: true, script: 'date +%Y%m%d%H%m%s'
     sonarDate = sonarDate - '\n'
-    sh '''
+    sh """
     ${scannerHome}/bin/sonar-scanner  -Dsonar.host.url=${sonarServer}  \
+    ${scannerHome}/bin/sonar-scanner  -Dsonar.projectKey=${projectName}  \
     -Dsonar.projectKey=${projectName}  \
     -Dsonar.projectName=${projectName}  \
     -Dsonar.projectVersion=${sonarDate} \
@@ -20,5 +22,5 @@ def SonarScan(projectName, des, path) {
     -Dsonar.java.binaries=target/classes \
     -Dsonar.java.test.binaries=target/test-classes \
     -Dsonar.java.surefire.report=target/surefire-reports
-    '''
+    """
 }
