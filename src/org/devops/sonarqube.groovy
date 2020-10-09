bfb04a8c0c6c8425392af4de257e3c00c2e9cf44
qin.zehua <775935087@qq.com>
2020-10-09 20:11:36 +0800

    使用sonqube的jenkins插件扫描

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -1,26 +1,27 @@
 package org.devops
 
 def SonarScan(projectName, des, path) {
-    // withSonarQubeEnv('sonarqube-test')
-    def scannerHome = '/usr/local/sonar-scanner-4.4.0.2170-linux/'
-    def sonarServer = 'http://192.168.0.102:9000'
-    sonarDate = sh returnStdout: true, script: 'date +%Y%m%d%H%m%s'
-    sonarDate = sonarDate - '\n'
-    sh """
-    ${scannerHome}/bin/sonar-scanner  -Dsonar.host.url=${sonarServer}  \
-    ${scannerHome}/bin/sonar-scanner  -Dsonar.projectKey=${projectName}  \
-    -Dsonar.projectKey=${projectName}  \
-    -Dsonar.projectName=${projectName}  \
-    -Dsonar.projectVersion=${sonarDate} \
-    -Dsonar.login=admin \
-    -Dsonar.password=admin \
-    -Dsonar.ws.timeout=30 \
-    -Dsonar.projectDescription=${des}  \
-    -Dsonar.links.homepage=http://www.baidu.com \
-    -Dsonar.sources=${path} \
-    -Dsonar.sourceEncoding=UTF-8 \
-    -Dsonar.java.binaries=target/classes \
-    -Dsonar.java.test.binaries=target/test-classes \
-    -Dsonar.java.surefire.report=target/surefire-reports
-    """
+    withSonarQubeEnv('sonarqube-test') {
+        def scannerHome = '/usr/local/sonar-scanner-4.4.0.2170-linux/'
+        // def sonarServer = 'http://192.168.0.102:9000'
+        sonarDate = sh returnStdout: true, script: 'date +%Y%m%d%H%m%s'
+        sonarDate = sonarDate - '\n'
+        sh """
+            #${scannerHome}/bin/sonar-scanner  -Dsonar.host.url=${sonarServer}  \
+            ${scannerHome}/bin/sonar-scanner  -Dsonar.projectKey=${projectName}  \
+            -Dsonar.projectKey=${projectName}  \
+            -Dsonar.projectName=${projectName}  \
+            -Dsonar.projectVersion=${sonarDate} \
+            #-Dsonar.login=admin \
+            #-Dsonar.password=admin \
+            -Dsonar.ws.timeout=30 \
+            -Dsonar.projectDescription=${des}  \
+            -Dsonar.links.homepage=http://www.baidu.com \
+            -Dsonar.sources=${path} \
+            -Dsonar.sourceEncoding=UTF-8 \
+            -Dsonar.java.binaries=target/classes \
+            -Dsonar.java.test.binaries=target/test-classes \
+            -Dsonar.java.surefire.report=target/surefire-reports
+            """
+    }
 }
